d52b6dd78b766652f71aa57155d1317325c40a8c
shay dayan <shay.etl.dayan@gmail.com>
2022-09-14 04:35:04 +0300

    continue working - found some bugs

---
+++ src_ui/src_pages/store_page.py
@@ -0,0 +1,41 @@
+import time
+from src_ui.src_pages.base_page import Base_Page
+from src_ui.src_drivers.driver_config import Meted, Driver
+
+
+class StorePage(Base_Page):
+    def __init__(self, driver: Driver):
+        super().__init__(driver)
+
+    _locations = {"book-container": (Meted.CLASS_NAME, "book-container"),
+                  "card_group": (Meted.CLASS_NAME, "card-group"),
+                  "card_footer": (Meted.CLASS_NAME, "card-footer"),
+                  "buy": (Meted.TAG_NAME, "button")}
+
+    def get_card_group(self):
+        card_group = self._driver.get_element(self._locations["card_group"])
+        books = self._driver.get_elements(self._locations["book-container"],card_group)
+        return books
+
+    # def get_book_title(self,book):
+    #     self._driver.get_element("title location",book)
+    #
+    # def get_book_by_title(self,title):
+    #     books = self.get_book_container()
+    #     for book in books:
+    #         if self.get_book_title(book) == title:
+    #             return book
+    #     return None
+
+    def click_buy(self, book):
+        buy_button = self._driver.get_element(self._locations["buy"], book)
+        flag = True
+        i = 0
+        while flag or i == 3:
+            try:
+                buy_button.click()
+            except:
+                buy_button.click()
+                i+=1
+                flag = False
+        self._driver.alerts_hendler()
